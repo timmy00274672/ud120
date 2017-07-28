bfd2af1e4daf62ca56265eabd157ced40910290a
timmy00274672 <timmy00274672@gmail.com>
2017-07-28 08:43:02 +0800

    add lesson 2 quiz 20

---
+++ Lesson 2/20 Quiz Calculating NB Accuracy/classify.py
@@ -0,0 +1,14 @@
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
+    """ compute the accuracy of your Naive Bayes classifier """
+    ### import the sklearn module for GaussianNB
+    from sklearn.naive_bayes import GaussianNB
+
+    ### create classifier
+    clf = GaussianNB()
+
+    ### fit the classifier on the training features and labels
+    clf.fit(features_train, labels_train)
+
+    ### use the trained classifier to predict labels for the test features
+    accuracy = clf.score(features_test, labels_test)
+    return accuracy
