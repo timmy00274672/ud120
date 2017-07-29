80182f1deaa9bbfc014b396ee3671a2119c8c801
timmy00274672 <timmy00274672@gmail.com>
2017-07-29 08:32:35 +0800

    add lesson 3 quiz 33

---
+++ Lesson 3/33 Quiz Optimize C Parameter/svm_author_id.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
+
+
+
+
+#########################################################
+### your code goes here ###
+from sklearn.svm import SVC
+for c in [10, 100, 1000, 10000]:
+    clf = SVC(C=c)
+    clf.fit(features_train, labels_train) 
+    pred = clf.predict(features_test)
+    from sklearn.metrics import accuracy_score
+    acc = accuracy_score(pred, labels_test)
+    print 'c = {}, acc = {}'.format(c, acc)
